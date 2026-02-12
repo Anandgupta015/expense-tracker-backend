b19d06890181fb7021481286c698a2487c1f18e5
Anand Gupta <anandgupta010106@gmail.com>
2026-02-12 12:02:50 +0530

    fix User mongoose model

--- models/User.js
+++ models/User.js
@@ -1,12 +1,21 @@
-import mongoose from 'mongoose';
+const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema(
-  {
-   name: String,
-  email: String,
-  password: String,
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
   },
-  { timestamps: true }
-);
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
 
-export default mongoose.model('User', UserSchema);
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
